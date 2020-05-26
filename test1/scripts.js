dca10f6a55f6c47055ee1ec3b39ace0f44d1d24a
Rafael Costa Palma <rafael.palma@opah.com.br>
2020-05-25 22:03:30 -0300

    solution

--- test1/scripts.js
+++ test1/scripts.js
@@ -1,3 +1,31 @@
-window.onload = function() {
-  console.log('paste your code here')
-}
+  function solution(A) {
+
+    let maxMultipleItem = -10000;
+
+    A.forEach(item => {
+
+      if (item > 10000) { 
+        return
+      }
+
+      if (item < -10000) {
+        return
+      }
+      
+      if (!isMultipleOfThree(item)) {
+        return
+      }
+      
+      if (item > maxMultipleItem) {
+        maxMultipleItem = item;
+      }
+      
+    })
+
+    return maxMultipleItem;
+
+  };
+
+  function isMultipleOfThree(value) {
+    return value % 3 === 0
+  }
